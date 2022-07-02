921b5054462a0194c1ab062e2538035d42107a16
Elide <zavalavinagreelide@gmail.com>
2022-07-02 16:02:33 -0500

    creamos y mofificamos estilos juntos con los componentes

--- src/elementos/Formularios.js
+++ src/elementos/Formularios.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const colores = {
@@ -23,6 +23,13 @@ const Label = styled.label`
   padding: 10px;
   min-height: 40px;
   cursor: pointer;
+
+  ${(props) =>
+    props.valido === "false" &&
+    css`
+      // Cambiamos el color del texto.
+      color: ${colores.error};
+    `}
 `;
 
 const GrupoInput = styled.div`
@@ -45,6 +52,19 @@ const Input = styled.input`
     outline: none;
     box-shadow: 3px 0px 30px rgba(163, 163, 163, 0.4);
   }
+
+  // accedemos a las propiedades del input que estamos creando.
+  ${(props) =>
+    props.valido === "true" &&
+    css`
+      border: 3px solid transparent;
+    `}
+
+  ${(props) =>
+    props.valido === "false" &&
+    css`
+      border: 3px solid ${colores.error} !important;
+    `}
 `;
 
 const LeyendaError = styled.p`
@@ -52,6 +72,18 @@ const LeyendaError = styled.p`
   margin-bottom: 0;
   color: ${colores.error};
   display: none;
+
+  ${(props) =>
+    props.valido === "true" &&
+    css`
+      display: none;
+    `}
+
+  ${(props) =>
+    props.valido === "false" &&
+    css`
+      display: block;
+    `}
 `;
 
 const IconoValidacion = styled(FontAwesomeIcon)`
@@ -61,6 +93,20 @@ const IconoValidacion = styled(FontAwesomeIcon)`
   z-index: 100;
   font-size: 1rem;
   opacity: 0;
+
+  ${(props) =>
+    props.valido === "false" &&
+    css`
+      opacity: 1;
+      color: ${colores.error};
+    `}
+
+  ${(props) =>
+    props.valido === "true" &&
+    css`
+      opacity: 1;
+      color: ${colores.exito};
+    `}
 `;
 
 const ContenedorTerminos = styled.div`
